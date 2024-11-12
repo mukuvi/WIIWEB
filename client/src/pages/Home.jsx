93bf1d8cfa5639d43dd518503fab1bf187010df9
jmngandu <jamesmngandu@gmail.com>
2024-11-12 11:06:12 +0300

    wiiweb

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <div className="navs"></div>
+    </>
+  );
 }
