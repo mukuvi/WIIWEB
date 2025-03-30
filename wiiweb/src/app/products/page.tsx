93a20ad2aa11df7910ff4101c5f1ccc8ef386236
mukuvi <jamesmngandu@gmail.com>
2025-03-30 13:42:33 +0300

    wiiweb

--- wiiweb/src/app/products/page.tsx
+++ wiiweb/src/app/products/page.tsx
@@ -6,7 +6,9 @@ export default function products() {
       <h1>products page</h1>
       <Link href="/products/1">products 1 </Link>
       <Link href="/products/2">products 2 </Link>
-      <Link href="/products/3">products 3 </Link>
+      <Link href="/products/3" replace>
+        products 3
+      </Link>
     </div>
   );
 }
